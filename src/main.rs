829d54d12e5e6c07262632c3070b4cc085e91db2
Matt McGivney <matt@mattmcgivney.com>
2024-01-14 17:34:26 -0700

    Client credentials grant with env vars

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-
+use reqwest;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::env;
 
-/// This is a made-up example. Requests come into the runtime as unicode
-/// strings in json format, which can map to any structure that implements `serde::Deserialize`
-/// The runtime pays no attention to the contents of the request payload.
+use tokio;
+
 #[derive(Deserialize)]
+// No Request format required at this time
 struct Request {
-    command: String,
+    // Example:
+    // command: String,
 }
 
 /// This is a made-up example of what a response structure may look like.
@@ -20,19 +23,35 @@ struct Response {
     msg: String,
 }
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
 async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
-    // Extract some useful info from the request
-    let command = event.payload.command;
+    // Set up request values
+    let client_id = env::var("CLIENT_ID")?;
+    let client_secret = env::var("CLIENT_SECRET")?;
+    let request_url = env::var("REQUEST_URL")?;
+
+    // Set up some data
+    let data = json!({
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "audience": "client-credentials-grant",
+        "grant_type": "client_credentials"
+    });
+
+    let client = reqwest::Client::new();
+
+    // Make the request
+    let response = client
+        .post(request_url)
+        .json(&data)
+        .send()
+        .await?
+        .text()
+        .await?;
 
     // Prepare the response
     let resp = Response {
         req_id: event.context.request_id,
-        msg: format!("Command {}.", command),
+        msg: format!("Body {}.", response),
     };
 
     // Return `Response` (it will be serialized to JSON automatically by the runtime)
